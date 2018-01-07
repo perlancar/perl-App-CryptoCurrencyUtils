fb62431f88e275cd633b68011230c6e68ab3094e
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-07 09:44:31 +0700

    Initial commit

---
+++ lib/App/CryptoCurrencyUtils.pm
@@ -0,0 +1,20 @@
+package App::CryptoCurrencyUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to cryptocurrencies
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::CoinMarketCapUtils>
+
+=cut
