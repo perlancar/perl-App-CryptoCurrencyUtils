9db2a50d1bec5e496409f9e6150b4505f126bca5
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-12 07:23:45 +0700

    Add utility: cmc-currency

--- lib/App/CryptoCurrencyUtils.pm
+++ lib/App/CryptoCurrencyUtils.pm
@@ -3,6 +3,48 @@ package App::CryptoCurrencyUtils;
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{cmc_currency} = {
+    v => 1.1,
+    summary => 'Open CMC (coinmarketcap.com) currency page',
+    args => {
+        symbol_or_name => {
+            schema => 'cryptocurrency::symbol_or_name*',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub cmc_currency {
+    require CryptoCurrency::Catalog;
+    my %args = @_;
+
+    my $cat = CryptoCurrency::Catalog->new;
+
+    my $cur0 = $args{symbol_or_name}
+        or return [400, "Please specify symbol/name"];
+
+    my $cur;
+    {
+        eval { $cur = $cat->by_symbol($cur0) };
+        last if $cur;
+        eval { $cur = $cat->by_name($cur0) };
+        last if $cur;
+        return [404, "No such cryptocurrency symbol/name"];
+    }
+
+    require Browser::Open;
+    my $err = Browser::Open::open_browser(
+        "https://coinmarketcap.com/currencies/$cur->{safename}/");
+    return [500, "Can't open browser"] if $err;
+    [200];
+}
+
 1;
 # ABSTRACT: CLI utilities related to cryptocurrencies
 
